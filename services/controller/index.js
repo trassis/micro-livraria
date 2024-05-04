0a0f0db97ead9825db9f24edc63f582b2e457ace
Thiago Assis <essemeia@gmail.com>
2024-05-04 17:07:34 -0300

    Tarefa prática #1 - Microservices

--- services/controller/index.js
+++ services/controller/index.js
@@ -42,6 +42,20 @@ app.get('/shipping/:cep', (req, res, next) => {
     );
 });
 
+/*
+ * Procura um novo produto pela sua ID
+ */
+app.get('/product/:id', (req, res, next) => {
+    inventory.SearchProductByID({ id: req.params.id }, (err, product) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send({ error: 'something failed :(' });
+        } else {
+            res.json(product);
+        }
+    });
+});
+
 /**
  * Inicia o router
  */
